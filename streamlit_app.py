4bea1bcf04ce52cd11b46594589ee4aebdfb6e70
Ho Yeon Won <93546861+hoyeonwon@users.noreply.github.com>
2024-12-18 13:07:40 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.write("changes")
+st.write("changes - test")
